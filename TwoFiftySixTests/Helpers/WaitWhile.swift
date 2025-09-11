279f601cd099be3f29e369cfac7f09a58ad338cf
Matt Neuburg <matt@tidbits.com>
2025-09-11 14:49:35 -0700

    make waitWhile function act very much like WaitWhile macro did

--- TwoFiftySixTests/Helpers/WaitWhile.swift
+++ TwoFiftySixTests/Helpers/WaitWhile.swift
@@ -1,8 +1,21 @@
 import Testing
 
-func waitWhile(_ condition: () async throws -> Bool) async throws {
-    while try await condition() {
-        try? await Task.sleep(for: .seconds(0.01))
-        await Task.yield()
+func waitWhile(_ seconds: Double = 5, condition: () async throws -> Bool) async throws {
+    enum TimeOutError: Error {
+        case timedOut
     }
+    var timer: Task<Void, Error>?
+    var timedOut = false
+    timer = Task {
+        try await Task.sleep(for: .seconds(seconds))
+        timedOut = true
+    }
+    while try await condition() {
+        try await Task.sleep(for: .seconds(0.01))
+        await Task.yield()
+        if timedOut {
+            throw TimeOutError.timedOut
+        }
+    }
+    timer?.cancel()
 }
